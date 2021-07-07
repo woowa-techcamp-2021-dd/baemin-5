b7915afcac2affa81b623f96474b2a4465ead8ee
jjunyjjuny <jjuny546@gmail.com>
2021-07-07 16:44:25 +0900

    style:(#10) 회원가입 페이지 마크업

--- baemin/route.js
+++ baemin/route.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const TITLE = '배민마트';
+
 router.get('/', (req, res) => {
-  res.render('mainPage', { pageTitle: 'test' });
+  res.render('mainPage', { pageTitle: `${TITLE}` });
+});
+
+router.get('/', (req, res) => {
+  res.render('loginPage', { pageTitle: `${TITLE}` });
+});
+
+router.get('/agreement', (req, res) => {
+  res.render('agreementPage', { pageTitle: `${TITLE}-약관 동의` });
+});
+
+router.get('/phone', (req, res) => {
+  res.render('phonePage', { pageTitle: `${TITLE}-휴대전화` });
 });
 
 module.exports = router;
