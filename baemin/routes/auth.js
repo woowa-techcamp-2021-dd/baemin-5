194bce04dfcb42e6ef700992024010462d854610
jjunyjjuny <jjuny546@gmail.com>
2021-07-07 22:42:22 +0900

    feat:(#13) bcrypt를 활용한 비밀번호 암호화  회원가입 완료

--- baemin/routes/auth.js
+++ baemin/routes/auth.js
@@ -1,38 +1,39 @@
 const db = require('../db.js');
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
+
+const SALT_ROUND = 10;
 
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
   console.log('req.body : ', req.body);
   console.log('e, pw : ', email, password);
-  db.find(
-    {
-      phoneNumber: email,
-      password,
-    },
-    (err, savedData) => {
-      if (err) return;
-      console.log('saveData : ', savedData);
+  db.find({ email, password }, (err, savedData) => {
+    if (err) return;
+    console.log('saveData : ', savedData);
 
-      if (!savedData.length) {
-        console.log('그런거 없수');
-        res.redirect('/login');
-        return;
-      }
+    if (!savedData.length) {
+      console.log('그런거 없수');
+      res.redirect('/login');
+      return;
+    }
 
-      console.log('그런거 있슴');
-      res.redirect('/');
-    },
-  );
+    console.log('그런거 있슴');
+    res.redirect('/');
+  });
 });
 
 router.post('/signup', (req, res) => {
-  db.insert(req.body, (err, signupDB) => {
+  const { email, password, nickname, birth } = req.body;
+  const encryptPassword = bcrypt.hashSync(password, SALT_ROUND);
+
+  db.insert({ email, nickname, birth, password: encryptPassword }, (err, signupDB) => {
     if (err) {
       console.error(err);
       return;
     }
+    console.log('signupDB : ', signupDB);
     res.redirect('/login');
   });
 });
